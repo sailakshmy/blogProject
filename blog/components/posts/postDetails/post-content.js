36f1fbc7ec828da03380b0229f5c258615c87333
Sai <sailakshmy94@gmail.com>
2025-05-05 07:33:03 +0400

    Rendering images from Markdown

--- blog/components/posts/postDetails/post-content.js
+++ blog/components/posts/postDetails/post-content.js
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 const PostContent = ({ post }) => {
   // const DUMMY_POST = {
@@ -14,10 +15,41 @@ const PostContent = ({ post }) => {
   //   content: "# This is the markdown content for the first post",
   // };
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const custommRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={custommRenderers}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 };
